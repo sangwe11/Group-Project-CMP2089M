4ba2424e6efa5ae8be3d536096eb7ee0e5d9a52d
Jack Laurel <jackelq44@hotmail.co.uk>
2015-04-16 14:46:48 +0100

    Bug fixes.
    Corrected spelling mistakes on help page / enemy description pop ups.
    Also fixed bug with path indicator on game pause.

--- CyberDefence/Assets/Scripts/Help.cs
+++ CyberDefence/Assets/Scripts/Help.cs
@@ -17,7 +17,7 @@ public class Help : MonoBehaviour {
 
 		GUI.Label (new Rect (0.68f * Screen.width, 0.1f * Screen.height, 250, 120), "SpamMail \nSpam Mail can be a threat to computer systems as it will often contain harmful malware embedded in attachments, or links to phishing sites. Most Spam Mail will be sent from a bot immitating an email that you recognise.");
 		GUI.Label (new Rect (0.68f * Screen.width, 0.41f * Screen.height, 250, 120), "Virus \nViruses are used to infiltrate a computer system, often embedded in spam mail, intent on causing damage to the system. This could include corrupting sensitive data, transmitting data back to the author or crashing critical systems.");
-		GUI.Label (new Rect (0.68f * Screen.width, 0.76f * Screen.height, 250, 120), "DDOS (Denial-of-Service) \nDDOS attacks are used to overwhel, a computer ysstem or network with heigh amounts of traffic, with the intention to cause distruption and outage of the network.");
+		GUI.Label (new Rect (0.68f * Screen.width, 0.76f * Screen.height, 250, 120), "DDOS (Denial-of-Service) \nDDOS attacks are used to overwhelm a computer system or network with high amounts of traffic, with the intention to cause disruption or complete outage of the network.");
 
 		// Spam Mail can be a threat to computer systems as it will often contain harmful malware embedded in attachments, or links to phishing sites. Most Spam Mail will be sent from a bot immitating an email that you recognise. Weak against SpamFilters.
 		// Viruses are used to infiltrate a computer system, often embedded in spam mail, intent on causing damage to the system. This could include corrupting sensitive data, transmitting data back to the author or crashing critical systems. Weak against antivirus software.
